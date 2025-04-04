3d72b4f62d0e0a2e6959651bfa4a52f91bee01e3
Rudhran B <bkrudhran@gmail.com>
2025-04-04 16:42:32 +0530

    Add files via upload

---
+++ moviessecurity/src/test/java/com/movies/moviessecurity/MoviessecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.moviessecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviessecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
