318c5c40ef3f2723f4d5e15da36ebc06bab18415
Rudhran B <bkrudhran@gmail.com>
2025-04-13 19:21:49 +0530

    Delete src directory

--- src/main/java/com/event/p1/repository/paymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.event.p1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.event.p1.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
