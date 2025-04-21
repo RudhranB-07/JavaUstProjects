da825fc548f4e76b0b8035d5853a1e9d958c24e0
Rudhran B <bkrudhran@gmail.com>
2025-04-21 17:11:32 +0530

    Delete p1 directory

--- p1/src/main/java/com/event/p1/repository/locationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.event.p1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.event.p1.entity.Location;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
